f58f1120831b8d6565e938fd712b6aca8cfc022b
SiaKovalina <siakovalina@gmail.com>
2019-05-27 12:49:30 +0300

    Add header button for feed filters

--- src/components/common/header/AddUsersBtn.js
+++ src/components/common/header/AddUsersBtn.js
@@ -9,35 +9,52 @@ const mapStateToProps = (state) => {
 }
 
 class AddUsersBtn extends Component {
+  onAddUsersPress = () => this.props.navigation.navigate('AddUsers')
+
+  onFeedFilterPress = () => console.log('pressed')
+
   render() {
     const { navigation, isConnected } = this.props
     const isChatsScreen = navigation.getParam('isChatsScreen')
+    const isFeedScreen = navigation.getParam('isFeedScreen')
 
     return (
-      <View style={styles.container}>
-        {isChatsScreen &&
-          <TouchableOpacity
-            disabled={!isConnected}
-            onPress={() => navigation.navigate('AddUsers')}>
-            <Image 
-              style={styles.messageIcon}
+      <TouchableOpacity
+        style={styles.container} 
+        onPress={isChatsScreen ? this.onAddUsersPress : this.onFeedFilterPress}
+        disabled={!isConnected}>
+          {isChatsScreen &&
+            <Image
+              style={styles.addUsersIcon}
               source={require('../../../assets/icons/add-users.png')} />
-          </TouchableOpacity>        
-        }
-      </View>
+          }
+          {isFeedScreen &&
+            <Image
+              style={styles.filtersIcon}
+              source={require('../../../assets/icons/feed-filters.png')} />
+          }
+      </TouchableOpacity>
     )
   }
 }
 
 const styles = StyleSheet.create({
   container: {
-    paddingRight: 10, 
-    flexDirection: 'row'
+    paddingRight: 10,
+    justifyContent: 'center', 
+    alignItems: 'center'
   },
 
-  messageIcon: {
-    width: 35, 
+  addUsersIcon: {
+    width: 35,
+    marginBottom: -12,
     resizeMode: 'contain',
+  },
+
+  filtersIcon: {
+    width: 27,
+    height: 27,
+    resizeMode: 'contain'
   }
 })
 
