5f0dd8fe2564d80847b9790926ab1ee2cb92bf3d
SiaKovalina <siakovalina@gmail.com>
2019-06-18 16:17:57 +0300

    Add expo-permissions package

--- src/config/registerForPushNotificationsAsync.js
+++ src/config/registerForPushNotificationsAsync.js
@@ -1,4 +1,5 @@
-import { Permissions, Notifications } from 'expo'
+import { Notifications } from 'expo'
+import * as Permissions from 'expo-permissions'
 import { PUSH_ENDPOINT } from '../constants'
 import { AsyncStorage } from 'react-native'
 import Constants from 'expo-constants'
